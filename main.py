e3fad1afde08eebada15e2e5716609f54294b14c
Norbix <norbertnnn@gmail.com>
2024-05-14 12:31:19 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import Multiplication from your library
+from medium_multiply import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
